1c33dba94ba6f30ed08870721bb840438c9e2afe
jesus-higgins25 <ospino0210@outlook.com>
2024-08-20 20:26:37 -0500

    Eliminacion De Ktor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation ("com.google.android.gms:play-services-location:21.3.0")
-    implementation("io.ktor:ktor-client-core:2.0.0")
-    implementation("io.ktor:ktor-client-cio:2.0.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
